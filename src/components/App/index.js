be6b5b9456a7c823f72e387e70e53b9db3ac4d74
Gerard Rovira Sánchez <gerard.rovira.sanchez@gmail.com>
2019-01-02 12:58:26 +0100

    styled components

---
+++ src/components/App/index.js
@@ -0,0 +1,36 @@
+import React, { Component, Fragment } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+`;
+
+const AppContainer = styled.div`
+
+`;
+
+function App() {
+  return (
+    <Fragment>
+      <GlobalStyle></GlobalStyle>
+      <AppContainer>
+        <h1>It works!</h1>
+      </AppContainer>
+    </Fragment>
+  );
+}
+
+export default App;
