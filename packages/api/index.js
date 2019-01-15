d3d46b8c9bd5f764ee1e08c13ce13d9eddd278d5
Gerard Rovira Sánchez <gerard.rovira.sanchez@gmail.com>
2019-01-15 15:07:45 +0100

    nodemailer

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('./services/email');
 const express = require('express');
 const cors = require('cors');
 
---
+++ packages/api/services/email.js
@@ -0,0 +1,55 @@
+const { createTransport } = require('nodemailer');
+
+const {
+  EMAIL_HOST,
+  EMAIL_PORT,
+  EMAIL_SECURE,
+  EMAIL_USERNAME,
+  EMAIL_PASSWORD,
+  EMAIL_FROM,
+} = process.env;
+
+const transporter = createTransport({
+  host: EMAIL_HOST,
+  port: EMAIL_PORT,
+  secure: EMAIL_SECURE,
+  auth: {
+    user: EMAIL_USERNAME,
+    pass: EMAIL_PASSWORD,
+  },
+});
+
+async function send(to, subject, body) {
+  const options = {
+    to,
+    subject,
+    html: body,
+    from: EMAIL_FROM,
+  };
+
+  return transporter.sendMail(options);
+}
+
+// Run as `node index.js email foo@example.com` to send a test email on boot.
+const argIndex = process.argv.findIndex(key => key === 'email');
+if (argIndex !== -1) {
+  const to = process.argv[argIndex + 1];
+  const subject = 'It works'
+  const text = `
+  Hello,
+
+  This is a test email.
+  `;
+  const body = `
+  <pre>${text}</pre>
+  `;
+  
+  (async () => {
+    await send(to, subject, body);
+    console.info(`Test email succesfully sent to ${to}`);
+  })();
+}
+
+module.exports = {
+  send,
+};
