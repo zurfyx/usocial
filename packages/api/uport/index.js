7cb471141ceacb22382d2c124f2572ce3fc8516b
Gerard Rovira Sánchez <gerard.rovira.sanchez@gmail.com>
2019-01-16 10:13:56 +0100

    ngrok for dev uport callback url

--- packages/api/uport/index.js
+++ packages/api/uport/index.js
@@ -1,5 +1,6 @@
 const { Credentials } = require('uport-credentials');
 const { message } = require('uport-transports');
+const { host } = require('../utils/ngrok');
 
 const credentials = new Credentials({
   appName: 'uSocial Identity',
@@ -8,10 +9,11 @@ const credentials = new Credentials({
 });
 
 async function emailDisclosureRequest() {
+  const callbackUrl = process.env.REACT_APP_API || await host();
+  console.info(`Callback URL ${callbackUrl}`)
   const requestToken = await credentials.createDisclosureRequest({
     notifications: true,
-    // callbackUrl: `${process.env.REACT_APP_API}/callback`,
-    callbackUrl: 'https://da8e98d8.ngrok.io/callback',
+    callbackUrl: `${callbackUrl}/callback`,
   });
   const uri = message.util.paramsToQueryString(message.util.messageToURI(requestToken));
   console.info(uri)
---
+++ packages/api/utils/ngrok.js
@@ -0,0 +1,23 @@
+/**
+ * Ngrok should solely be used on a development environment for hooks, as a replacement for
+ * localhost:3001.
+ * For this reason it's lazy loaded and its dependency is listed in devDependencies.
+ */
+const PORT = process.env.PORT || 3001;
+
+let instance;
+async function host() {
+  if (instance) {
+    return instance;
+  }
+  instance = new Promise(async (resolve) => {
+    const ngrok = require('ngrok');
+    const newHost = await ngrok.connect(PORT);
+    resolve(newHost);
+  });
+  return instance;
+}
+
+module.exports = {
+  host,
+};
