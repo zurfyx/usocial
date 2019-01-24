b7d836c742ece78c5a4db0153b7de416c92a2f09
Gerard Rovira Sánchez <gerard.rovira.sanchez@gmail.com>
2019-01-24 10:34:15 +0100

    .

--- packages/client/src/common/ScrollToTop.js
+++ packages/client/src/common/ScrollToTop.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 
-function ScrollToTop({ history, children }) {
+function ScrollToTop({ history }) {
   useEffect(() => {
     const unlisten = history.listen(() => {
       window.scrollTo(0, 0);
