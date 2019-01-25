7d3e4392401b8f30f14d6087b5ad32e626216b6f
Gerard Rovira Sánchez <gerard.rovira.sanchez@gmail.com>
2019-01-25 10:50:14 +0100

    no connections

--- packages/client/src/connections/Connections.js
+++ packages/client/src/connections/Connections.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { connect } from '../utils/react-context';
 import Section from '../common/Section';
@@ -6,6 +7,8 @@ import SectionHeader1 from '../common/SectionHeader1';
 import BoxList from '../common/BoxList';
 import MaxWidth from '../common/MaxWidth';
 import ToolBox from '../common/ToolBox';
+import BoxListItem from '../common/BoxListItem';
+import DefaultButton from '../common/DefaultButton';
 import RefreshTool from '../shared/RefreshTool';
 import { UserContext } from '../app/UserProvider';
 import { spaces } from '../app/theme';
@@ -15,7 +18,19 @@ const ConnectionsContainer = styled(MaxWidth)`
   padding: ${spaces.section};
 `;
 
+const NoConnections = styled(BoxListItem)`
+  padding: 4.2rem;
+  text-align: center;
+`;
+
+const ConnectButton = styled(DefaultButton)`
+  display: block;
+  margin: 2rem auto;
+`;
+
 function Connections({ user }) {
+  const verified = user.user.verified;
+
   return (
     <ConnectionsContainer>
       <ToolBox>
@@ -24,7 +39,13 @@ function Connections({ user }) {
       <Section>
         <SectionHeader1 border={false}>Connections</SectionHeader1>
         <BoxList>
-          {user.user.verified && user.user.verified.map((verifiedItem) => (
+          {verified.length === 0 && (
+            <NoConnections>
+              No connections found.
+              <Link to="/dashboard/connect"><ConnectButton>Connect</ConnectButton></Link>
+            </NoConnections>
+          )}
+          {verified && verified.map((verifiedItem) => (
             <ConnectionsItem key={verifiedItem.jwt} verifiedItem={verifiedItem} />
           ))}
         </BoxList>
