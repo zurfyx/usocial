ec037baf766e509032a39fc5b7b3164768530f21
Gerard Rovira Sánchez <gerard.rovira.sanchez@gmail.com>
2019-01-26 11:50:32 +0100

    keyvalue responsive

--- packages/client/src/common/KeyValue.js
+++ packages/client/src/common/KeyValue.js
@@ -1,9 +1,23 @@
 import styled from 'styled-components';
+import { sizes } from '../app/theme';
 
 const KeyValuePair  = styled.dl`
   display: grid;
   grid-template-columns: 110px 1fr;
-  grid-gap: 0.5rem;
+  grid-column-gap: 4rem;
+  grid-row-gap: 0.55rem;
+
+  dd {
+    margin-left: 0;
+  }
+
+  @media (max-width: ${sizes.phablet}) {
+    display: inline;
+
+    dd {
+      margin-bottom: 1rem;
+    }
+  }
 `;
 
 export default KeyValuePair;
