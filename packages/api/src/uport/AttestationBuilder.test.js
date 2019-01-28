baa44c1bc4428f575a13480488611f9d34ef7d63
Gerard Rovira Sánchez <gerard.rovira.sanchez@gmail.com>
2019-01-28 13:08:13 +0100

    builder test

---
+++ packages/api/src/uport/AttestationBuilder.test.js
@@ -0,0 +1,66 @@
+const AttestationBuilder = require('./AttestationBuilder');
+
+test('adds one', () => {
+  const builder = new AttestationBuilder();
+  builder.addOne('email', 'foo@example.com');
+
+  expect(builder.values).toEqual({
+    'email': ['foo@example.com'],
+  });
+});
+
+test('adds many', () => {
+  const builder = new AttestationBuilder();
+  builder.addMany({
+    email: ['foo@example.com', 'bar@example.com'],
+    facebook: ['123'],
+  });
+
+  expect(builder.values).toEqual({
+    email: ['foo@example.com', 'bar@example.com'],
+    facebook: ['123'],
+  });
+});
+
+test('adds one and many', () => {
+  const builder = new AttestationBuilder()
+    .addOne('email', 'foo@example.com')
+    .addMany({ email: ['bar@example.com'] });
+
+  expect(builder.values).toEqual({
+    email: ['foo@example.com', 'bar@example.com'],
+  });
+});
+
+test('adds duplicated, only 1 is kept', () => {
+  const builder = new AttestationBuilder()
+    .addOne('email', 'foo@example.com')
+    .addMany({ email: ['foo@example.com', 'foo@example.com'] });
+
+  expect(builder.values).toEqual({
+    email: ['foo@example.com'],
+  });
+});
+
+test('values are stringified', () => {
+  const builder = new AttestationBuilder()
+    .addMany({ facebook: [123] });
+
+  expect(builder.values).toEqual({
+    facebook: ['123'],
+  });
+});
+
+test('add many as undefined makes no changes', () => {
+  const builder = new AttestationBuilder()
+    .addMany();
+  
+  expect(builder.values).toEqual({});
+});
+
+test('add many as empty object makes no changes', () => {
+  const builder = new AttestationBuilder()
+    .addMany();
+
+  expect(builder.values).toEqual({});
+})
